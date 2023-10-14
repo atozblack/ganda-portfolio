25ab47d53d06153735a15d907712a3e54810a5a2
Hasian Sugar Ishak <HassSugar@Hasians-MacBook-Pro.local>
2023-10-14 15:17:53 +0700

    first commit

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <div
+      className="parallax"
+      ref={ref}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, rgba(89,83,46,1) 0%, rgba(113,79,49,1) 23%, rgba(28,75,72,1) 65%, rgba(72,77,41,1) 85%, rgba(38,77,85,1) 100%)"
+            : "linear-gradient(180deg, rgba(89,83,46,1) 0%, rgba(113,79,49,1) 23%, rgba(28,75,72,1) 65%, rgba(72,77,41,1) 85%, rgba(38,77,85,1) 100%)",
+      }}
+    >
+      <motion.h1 style={{ y: yText }}>
+        {type === "services" ? "What My Skill?" : "What I Did?"}
+      </motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div
+        className="planets"
+        style={{
+          y: yBg,
+          backgroundImage: `url(${
+            type === "services" ? "/planets.png" : "/sun.png"
+          })`,
+        }}
+      ></motion.div>
+      <motion.div style={{ x: yBg }} className="stars"></motion.div>
+    </div>
+  );
+};
+
+export default Parallax;
